d9070f3ba968f18cba28889508fe16f6524518d1
BenEdwards-0612 <e021351l@student.staffs.ac.uk>
2024-02-22 12:22:18 +0000

    Added Filter
    Added Message to BP to show intention with asset

--- Content/Python/FillComboBox.py
+++ Content/Python/FillComboBox.py
@@ -5,7 +5,7 @@ exclude_folders = []
 combo_box = unreal.ComboBoxString()
 file_paths = []
 
-def FillCombo(combo_box, accepted_classes, exclude_folders) -> list[str]:
+def FillCombo(combo_box, excluded_folders, excluded_classes, excluded_names) -> list[str]:
 
     assetReg = unreal.AssetRegistryHelpers.get_asset_registry()
     allAssetsData = assetReg.get_assets_by_path("/Game/", True)
@@ -16,11 +16,27 @@ def FillCombo(combo_box, accepted_classes, exclude_folders) -> list[str]:
         filepath = assetString[filepathStartPos : filepathEndPos]
         assetClassStartPos = assetString.index('.') + 1
         assetClassEndPos = assetString.index(' ')
-        assetNameStartPos = assetString.index('.', assetClassEndPos + 1) + 1
-        unreal.log(assetString[assetNameStartPos : ] + " - " + assetString[assetClassStartPos : assetClassEndPos])
-        if assetString[assetClassStartPos : assetClassEndPos] in accepted_classes:
-            for folderName in exclude_folders:
-                if "/" + folderName + "/" not in assetString:
-                    combo_box.add_option(assetString[assetNameStartPos : ])
-                    file_paths.append(filepath)
+        assetClass = assetString[assetClassStartPos : assetClassEndPos]
+        assetName = assetString[assetString.index('.', assetClassEndPos + 1) + 1 : ]
+        flag = False
+
+        for folderName in excluded_folders:
+            if "/" + folderName + "/"  in filepath:
+                flag = True
+
+        if not flag:
+            for classFilter in excluded_classes:
+                if classFilter in assetClass:
+                    flag = True
+
+        if not flag:
+            for nameFilter in excluded_names:
+                if nameFilter  in assetName:
+                    flag = True
+                    
+        if not flag:
+            combo_box.add_option(assetName)
+            file_paths.append(filepath)
+            unreal.log("Name : " + assetName + "   Class : " + assetClass + "   FilePath : " + filepath)
+
     return file_paths
