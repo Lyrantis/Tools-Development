ef8c2e699f5afe95f20ac4aceefc0678325e3be7
BenEdwards-0612 <112256392+BenEdwards-0612@users.noreply.github.com>
2024-02-28 23:35:49 +0000

    Created function library class

---
+++ Source/ToolsDevelopment/AssetPaletteLibrary.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "AssetPaletteLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOOLSDEVELOPMENT_API UAssetPaletteLibrary : public UUserWidget
+{
+	GENERATED_BODY()
+
+	
+	
+};
