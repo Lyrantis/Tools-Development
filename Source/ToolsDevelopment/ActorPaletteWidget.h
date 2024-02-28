1d1430e4a11bbf2cc769c2c1179208ab31e932b4
BenEdwards-0612 <112256392+BenEdwards-0612@users.noreply.github.com>
2024-02-28 15:56:25 +0000

    Continued work - Arrows just need to move node
    Child index may work

---
+++ Source/ToolsDevelopment/ActorPaletteWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EditorUtilityWidget.h"
+#include "ActorPaletteWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOOLSDEVELOPMENT_API UActorPaletteWidget : public UEditorUtilityWidget
+{
+	GENERATED_BODY()
+	
+};
